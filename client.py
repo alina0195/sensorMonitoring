500f4177b21f4d24d9ea124b5f210bd44bd6b106
alina.gheorghe <alina.gheorghe@mta.ro>
2022-11-26 12:18:04 +0200

    Handling errors  + verificare input pentru inceperea unei noi runde pe parte de client

--- client.py
+++ client.py
@@ -1,5 +1,5 @@
 import socket
-
+import sys
 less_than_message = "Numarul este mai mic decat numarul ales"
 greater_than_message = "Numarul este mai mare decat numarul ales"
 success_message ="Numarul este corect"
@@ -13,57 +13,89 @@ def int_to_bytes(integer):
 def negative_int_to_bytes(integer):
     return integer.to_bytes(1, byteorder='big', signed=True)
 
-clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-host = socket.gethostname()                           
-port = 9999
-clientsocket.connect((host, port))                               
 
-wellcome_message = clientsocket.recv(1024).decode("utf-8")
-print (wellcome_message)
+try:
+    clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+except socket.error as e:
+    print ("Error creating socket: %s" % e)
+    sys.exit(1)
 
-start_message = clientsocket.recv(1024).decode("utf-8")
-print (start_message)
+try:
+    host = socket.gethostname()                           
+    port = 9999
+    clientsocket.connect((host, port))   
+except socket.gaierror as e:
+    print ("Address-related error connecting to server: %s" % e) 
+    sys.exit(1)
+except socket.error as e: 
+    print ("Connection error: %s" % e) 
+    sys.exit(1)                  
+
+
+try:
+    wellcome_message = clientsocket.recv(1024).decode("utf-8")
+    print (wellcome_message)
+except socket.error as e: 
+    print (f'Error receiving data: {e}') 
+    sys.exit(1)
+
+try:
+    start_message = clientsocket.recv(1024).decode("utf-8")
+    print (start_message)
+except socket.error as e: 
+    print (f'Error receiving data: {e}') 
+    sys.exit(1)
 
 running=True
 
-while running:
-    
-    value_tried = None
-    while True:
-        value_tried = input(">> ")
-        if value_tried.isnumeric():
-            break
-        else:
-            print('Invalid. Introduceti un numar valid!')
-            continue
-    
-    #ii trimite cu certitudine un numar valid 
-    clientsocket.send(int_to_bytes(int(value_tried)))
-    
-    if running: #a introdus un numar valid
-        feedback_message = clientsocket.recv(1024).decode("utf-8")
-        print(feedback_message)
-    
-        if feedback_message.startswith(success_message):
+try:
+    while running:
         
-            # receive question for another round
-            print(clientsocket.recv(1024).decode("utf-8"))
-            agreement = input(">>")
-            clientsocket.send(string_to_bytes((agreement)))
-            
-            ## evalueaza raspunsul pt o noua runda
-            if agreement=="nu":
+        value_tried = None
+        while True:
+            value_tried = input(">> ")
+            if value_tried.isnumeric():
                 break
             else:
-                running=True
+                print('Invalid. Introduceti un numar valid!')
                 continue
+        
+        #ii trimite cu certitudine un numar valid 
+        clientsocket.send(int_to_bytes(int(value_tried)))
+        
+        if running: #a introdus un numar valid
+            feedback_message = clientsocket.recv(1024).decode("utf-8")
+            print(feedback_message)
+        
+            if feedback_message.startswith(success_message):
             
-        elif feedback_message==less_than_message or feedback_message==greater_than_message:
-            running=True
-        else:
-            print("Eroare din partea serverului. Jocul se incheie...")
-            break
+                # receive question for another round
+                print(clientsocket.recv(1024).decode("utf-8"))
+                agreement = None
+                while True:
+                    agreement = input(">>")
+                    if agreement.lower().strip()=="nu" or agreement.lower().strip()=="da":
+                        break
+                    else:
+                        print("Raspuns invalid")
+                        continue
+                clientsocket.send(string_to_bytes((agreement)))
+                
+                ## evalueaza raspunsul pt o noua runda
+                if agreement.lower().strip()=="nu":
+                    break
+                elif agreement.lower().strip()=="da":
+                    running=True
+                    continue
+                
+            elif feedback_message==less_than_message or feedback_message==greater_than_message:
+                running=True
+            else:
+                print("Eroare din partea serverului. Jocul se incheie...")
+                break
 
-   
-print(clientsocket.recv(1024).decode("utf-8")) # scorul sau
-clientsocket.close()
+    print(clientsocket.recv(1024).decode("utf-8")) # scorul sau
+except socket.error as e:
+    print(f'Erori in comunicarea cu serverul')
+finally:
+    clientsocket.close()
--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import socket                                         
 import random
+import sys
 
 welcome_message = "Bun venit! Daca doresti sa parasesti jocul tasteaza litera q."
 success_message = "Numarul este corect"
@@ -19,11 +20,23 @@ def generate_random_number():
     print(f'Am generat: {nb}')
     return nb
 
+try:
+    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+except socket.error as e:
+     print ("Error creating socket: %s" % e)
+     sys.exit(1)
 
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-host = socket.gethostname() 
-port = 9999                                           
-serversocket.bind((host, port))  
+try:
+    host = socket.gethostname() 
+    port = 9999                                           
+    serversocket.bind((host, port)) 
+except socket.gaierror as e:
+    print ("Address-related error connecting to server: %s" % e) 
+    sys.exit(1)
+except socket.error as e: 
+    print ("Connection error: %s" % e) 
+    sys.exit(1) 
+    
 serversocket.listen(2) 
 generated_number = generate_random_number()
 
@@ -34,43 +47,57 @@ min_score = 100000
 
 clientsocket1,addr1 = serversocket.accept()      
 print("Conexiune de la %s" % str(addr1))
-clientsocket1.send(string_to_bytes(welcome_message))
-clientsocket1.send(string_to_bytes(start_message))
 
+try:
+    clientsocket1.send(string_to_bytes(welcome_message))
+except socket.error as e: 
+    print (f'Error sending data: {e}') 
+    sys.exit(1)
+try:
+    clientsocket1.send(string_to_bytes(start_message))
+except socket.error as e: 
+    print (f'Error sending data: {e}') 
+    sys.exit(1)
 
-while True:
-    received_number = int.from_bytes(clientsocket1.recv(1024), "big")
-    print(f'Am primit {received_number} ')
+try:
+    while True:
+        try:
+            received_number = int.from_bytes(clientsocket1.recv(1024), "big")
+            print(f'Am primit {received_number} ')
+        except socket.error as e:
+            print(f'Error receiving data: {e}')
 
-    current_score +=1
-    
-    if received_number == generated_number:
-        clientsocket1.send(string_to_bytes(success_message))
-        if min_score > current_score:
-            min_score=current_score
+        current_score +=1
+        
+        if received_number == generated_number:
+            clientsocket1.send(string_to_bytes(success_message))
+            if min_score > current_score:
+                min_score=current_score
+                
+            # intreaba daca vrea inca o runda
+            clientsocket1.send(string_to_bytes("O noua runda? Testeaza da/nu"))
             
-        # intreaba daca vrea inca o runda
-        clientsocket1.send(string_to_bytes("O noua runda? Testeaza da/nu"))
-        
-        # primeste raspuns
-        agreement = clientsocket1.recv(1024).decode('utf-8')
-        
-        ##evalueaza raspunsul pt o noua runda
-        # daca da => genereaza un nou nr random + continue
-        if agreement.startswith("da"):
-            generated_number = generate_random_number()
-            current_score=0
-            continue
+            # primeste raspuns
+            agreement = clientsocket1.recv(1024).decode('utf-8')
+            
+            ##evalueaza raspunsul pt o noua runda
+            # daca da => genereaza un nou nr random + continue
+            if agreement.startswith("da"):
+                generated_number = generate_random_number()
+                current_score=0
+                continue
+            else:
+                break
+            # daca nu => break
+        elif received_number < generated_number:
+            clientsocket1.send(string_to_bytes(greater_than_message))
         else:
-            break
-        # daca nu => break
-    elif received_number < generated_number:
-        clientsocket1.send(string_to_bytes(greater_than_message))
-    else:
-        clientsocket1.send(string_to_bytes(less_than_message))
+            clientsocket1.send(string_to_bytes(less_than_message))
+
+    clientsocket1.send(string_to_bytes('\nScorul tau maxim: ' + str(min_score)))
     
-    
-   
-clientsocket1.send(string_to_bytes('\nScorul tau maxim: ' + str(min_score)))
-clientsocket1.close()
+except socket.error as e:
+    print(f'Erori in comunicarea cu clientul')
+finally:
+    clientsocket1.close()
 
